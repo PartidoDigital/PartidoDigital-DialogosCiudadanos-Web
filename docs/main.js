28b33e5832c7e8c0fb8979c3bc27df860e13186a
Leo Giovanetti <hello@leog.me>
2021-05-08 13:41:53 -0300

    Chequeo de datos de form

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,8 @@
 jQuery(function () {
 	$("button[type=submit]").bind("click", function () {
-    var $form = $(this).parents("form");
-    var opcionesParticipacion = [];
-    $("[name='participacion']:checked", $form).map((i, v) => opcionesParticipacion.push($(v).val()));
+		var $form = $(this).parents("form");
+		var opcionesParticipacion = [];
+		$("[name='participacion']:checked", $form).map((i, v) => opcionesParticipacion.push($(v).val()));
 		$.ajax({
 			method: "post",
 			url: "https://info.partidodigital.org.uy/form/submit?ajax=true",
@@ -12,31 +12,42 @@ jQuery(function () {
 				"mauticform[nombre]": $("[name=nombre]", $form).val(),
 				"mauticform[apellido]": $("[name=apellido]", $form).val(),
 				"mauticform[email]": $("[name=email]", $form).val(),
-        "mauticform[participacion][]": opcionesParticipacion,
+				"mauticform[participacion][]": opcionesParticipacion,
 				"mauticform[submit]": 1,
 				"mauticform[formId]": 7,
 				"mauticform[formName]": "dialogosciudadanos",
 				"mauticform[return]": ""
 			}),
 			beforeSend: function () {
-				$("button[type=submit]", $form).prop('disabled', true);
-        $("button[type=submit]", $form).html('Enviando...');
+				if ($("[name=nombre]", $form).val() === "" || $("[name=apellido]", $form).val() === "" ||
+					$("[name=email]", $form).val() === "" || opcionesParticipacion.length == 0) {
+						$("button[type=submit]", $form).prop('disabled', true);
+						$("button[type=submit]", $form).html('Asegúrese de llenar todo el formulario');
+						setTimeout(function () {
+							$("button[type=submit]", $form).html('Registrarme en Diálogos Ciudadanos');
+							$("button[type=submit]", $form).prop('disabled', false);
+						}, 3000);
+						return false;
+				} else {
+					$("button[type=submit]", $form).prop('disabled', true);
+					$("button[type=submit]", $form).html('Enviando...');
+				}				
 			},
 			success: function () {
 				$("button[type=submit]", $form).html('¡Enviado! Gracias por participar.');
 				$("input[type=text], input[type=email]", $form).val("");
-        $("[name='participacion']:checked").prop('checked',false);
-        setTimeout(function() {
-          $("button[type=submit]", $form).html('Registrarme en Diálogos Ciudadanos');
-          $("button[type=submit]", $form).prop('disabled', false);
-        }, 5000)
+				$("[name='participacion']:checked").prop('checked', false);
+				setTimeout(function () {
+					$("button[type=submit]", $form).html('Registrarme en Diálogos Ciudadanos');
+					$("button[type=submit]", $form).prop('disabled', false);
+				}, 5000)
 			},
-			error: function (jqXHR, textStatus, errorThrown) {
+			error: function () {
 				$("button[type=submit]", $form).html('Hubo un error al enviar tus datos. Intenta luego.');
-        setTimeout(function() {
-          $("button[type=submit]", $form).html('Registrarme en Diálogos Ciudadanos');
-          $("button[type=submit]", $form).prop('disabled', false);
-        }, 5000)
+				setTimeout(function () {
+					$("button[type=submit]", $form).html('Registrarme en Diálogos Ciudadanos');
+					$("button[type=submit]", $form).prop('disabled', false);
+				}, 5000)
 			}
 		});
 	});
